23e6ad78d66c703b8a479b154091442d13207ede
Salih Gireniz <salihgireniz@moomoo.games>
2022-11-08 17:12:58 +0300

    Templates Initialized

---
+++ Assets/_Game/_Scripts/_Managers/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        //LevelManager.Instance.LoadLevel();
+    }
+}
